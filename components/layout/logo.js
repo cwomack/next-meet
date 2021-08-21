048014834b8a0cb9af3b48afdeeb8e918994765f
WomackCodes <chris@womack.codes>
2021-08-20 18:54:52 -0700

    refactor repository

--- components/layout/logo.js
+++ components/layout/logo.js
@@ -1,7 +1,7 @@
-import classes from "./logo.module.css";
+import classes from './logo.module.css';
 
 function Logo() {
-  return <div className={classes.logo}>Chris logo.js layout</div>;
+  return <div className={classes.logo}>Max' Next Blog</div>;
 }
 
 export default Logo;
